79a2301c7d776bea91eff76c445e52806d86b383
Eric Allen <eric.allen@usu.edu>
2017-10-06 16:16:06 -0600

    updated config files for 9.6

--- WEB-INF/classes/EmployeeSelfService_configuration.groovy
+++ WEB-INF/classes/EmployeeSelfService_configuration.groovy
@@ -150,8 +150,8 @@ grails.plugin.springsecurity.saml.metadata.sp.defaults = [
 //                       +++ LOGGER CONFIGURATION +++
 //
 // ******************************************************************************
-String loggingFileDir = (System.getenv('CATALAINA_HOME') ?: '/target')
-String loggingFileName = "${loggingFileDir}/logs/${logAppName}.log".toString()
+String loggingFileDir =  (System.getenv('CATALAINA_HOME') ?: '/target')
+String loggingFileName = "${loggingFileDir}/${logAppName}.log".toString()
 
 
 // Note that logging is configured separately for each environment ('development', 'test', and 'production').
@@ -276,22 +276,22 @@ grails.resources.adhoc.excludes = ['/WEB-INF/**']
 
 
 // Location of employer logos for the Pay Stub PDF.
-banner.hr.employerLogoPath = (System.getenv('BANNER_HR_EMPLOYERLOGOPATH') ?: 'http://<host_name>:<port_number>/xxxx')
+banner.hr.employerLogoPath = 'http://<host_name>:<port_number>/xxxx'
 
 // Location of Apache FOP base directory for generating PDF documents.
 // Leave blank to use the default location.
-banner.pdf.fopBaseUrl = (System.getenv('BANNER_PDF_FOPBASEDURL') ?: '')
+banner.pdf.fopBaseUrl = ''
 
 banner8.SS.addressUpdateURL = "bwgkogad.P_SelectAtypUpdate"
-banner8.SS.addressUpdate = 'Y'
+banner8.SS.addressUpdate = 'N'
 banner8.SS.emailUpdateURL = "bwgkogad.P_SelectEmalUpdate"
-banner8.SS.emailUpdate = 'Y'
+banner8.SS.emailUpdate = 'N'
 banner8.SS.emergencyContactUpdateURL = "bwgkoemr.P_SelectEmrgContacts"
-banner8.SS.emergencyContactUpdate = 'Y'
+banner8.SS.emergencyContactUpdate = 'N'
 
 //Benefits links HR Dashboard
 banner8.SS.benefitsEnrollmentURL = "bwpkdsta.P_ShowEnrollmentMenu"
-banner8.SS.benefitsEnrollment8xLinkAvailable = 'N'
+banner8.SS.benefitsEnrollment8xLinkAvailable = 'Y'
 banner8.SS.currentBeneficiariesURL = "bwpkdbcv.P_NamesAndBenefits"
 banner8.SS.currentBeneficiaries8xLinkAvailable = 'Y'
 banner8.SS.currentSummaryURL = "bwpkebst.P_DispIDSelect"
@@ -299,13 +299,13 @@ banner8.SS.currentSummary8xLinkAvailable = 'Y'
 
 //My Activities links HR Dashboard
 banner8.SS.enterTimeURL = "bwpktais.P_SelectTimeSheetRoll"
-banner8.SS.enterTime8xLinkAvailable = 'N'
+banner8.SS.enterTime8xLinkAvailable = 'Y'
 banner8.SS.requestTimeOffURL = "bwpktais.P_SelectLeaveRequestRoll"
-banner8.SS.requestTimeOff8xLinkAvailable = 'N'
+banner8.SS.requestTimeOff8xLinkAvailable = 'Y'
 banner8.SS.electronicPersonalActionFormsURL = "bwpkepaf.P_DispEpafMenu"
 banner8.SS.electronicPersonalActionForms8xLinkAvailable = 'Y'
 banner8.SS.facultyLoadCompensationURL = "twbkwbis.P_GenMenu?name=pmenu.P_FacMenu"
-banner8.SS.facultyLoadCompensation8xLinkAvailable = 'N'
+banner8.SS.facultyLoadCompensation8xLinkAvailable = 'Y'
 banner8.SS.salaryPlannerURL = "twbkwbis.P_GenMenu?name=pmenu.P_SalaMenu"
 banner8.SS.salaryPlanner8xLinkAvailable = 'Y'
 banner8.SS.effortReportingURL = "bwpkolib.p_launch_flex"
@@ -319,18 +319,18 @@ banner8.SS.benefitsAdministrator8xLinkAvailable = 'Y'
 banner8.SS.morePersonalInfoUpdateURL = "twbkwbis.P_GenMenu?name=bmenu.P_GenMnu"
 banner8.SS.morePersonalInfoUpdate = 'Y'
 banner8.SS.timeSheetURL = "bwpktais.P_SelectTimeSheetRoll"
-banner8.SS.timeSheet8xLinkAvailable = 'N'
+banner8.SS.timeSheet8xLinkAvailable = 'Y'
 banner8.SS.leaveApprovalsURL = "bwpktais.P_SelectLeaveReportRoll"
-banner8.SS.leaveApprovals8xLinkAvailable = 'N'
+banner8.SS.leaveApprovals8xLinkAvailable = 'Y'
 banner8.SS.campusDirectoryURL = "bwpkedir.P_DisplayDirectory"
-banner8.SS.campusDirectory8xLinkAvailable = 'N'
+banner8.SS.campusDirectory8xLinkAvailable = 'Y'
 banner8.SS.employeeMenuURL = "twbkwbis.P_GenMenu?name=pmenu.P_MainMnu"
-banner8.SS.employeeMenuLinkAvailable = 'Y'
+banner8.SS.employeeMenuLinkAvailable = 'N'
 
 
 //Tax links HR Dashboard
 banner8.SS.electronicW2ConsentURL = "bwpkxtxs.P_W2Consent"
-banner8.SS.electronicW2Consent8xLinkAvailable = 'N'
+banner8.SS.electronicW2Consent8xLinkAvailable = 'Y'
 banner8.SS.w2WageTaxStatementURL = "bwpkxtxs.P_ChooseW2Key"
 banner8.SS.w2WageTaxStatement8xLinkAvailable = 'Y'
 banner8.SS.w2CorrectedWageTaxStatementURL = "bwpkxtxs.P_ChooseW2cKey"
@@ -362,20 +362,20 @@ banner8.SS.openEnrollmentNewHire8xLinkAvailable = 'Y'
 banner8.SS.openEnrollmentURL = "bwpkdsta.P_ShowEnrollmentMenu"
 banner8.SS.openEnrollment8xLinkAvailable = 'Y'
 banner8.SS.lifeEventChangeURL = "bwpkdsta.P_ShowEnrollmentMenu"
-banner8.SS.lifeEventChange8xLinkAvailable = 'N'
+banner8.SS.lifeEventChange8xLinkAvailable = 'Y'
 
 //Position Description Link
-banner.SS.positionDescriptionLinkAvailable = 'N'
+banner.SS.positionDescriptionLinkAvailable = 'Y'
 
 //Direct Deposit Application Link.  NOTE - please disable the 8x direct deposit link if this one is being enabled.
 // Use Consolidated Banner General SSB App Name if Consolidated General App is available, else use standalone
 // DirectDeposit application URL
-banner.SS.directDepositURL = (System.getenv('BANNER_SS_DIRECTDEPOSITURL') ?: 'http://<host_name>:<port_number>/<DirectDepositAppName>/')
-banner.SS.directDepositAppLinkAvailable = 'Y'
+banner.SS.directDepositURL = 'http://<host_name>:<port_number>/<DirectDepositAppName>/'
+banner.SS.directDepositAppLinkAvailable = 'N'
 
 
 //Personal Info Application Link.
-banner.SS.personalInfoURL = (System.getenv('BANNER_SS_PERSONALINFOURL') ?: 'http://<host_name>:<port_number>/<GENERALSELFSERVICE>/')
+banner.SS.personalInfoURL = 'http://<host_name>:<port_number>/<GeneralAppName>/ssb/personalInformation#/personalInformationMain'
 banner.SS.personalInfoAppLinkAvailable = 'Y'
 
 //Deductions History Link.
@@ -430,18 +430,20 @@ ess.displayComponent.HireDate  = 'Y'
 /******************************************************************************
 *Configuration to use themes served by the Theme Server*                                                                              *
 ***************************************************************************** **/
-banner.them.url=(System.getenv('BANNER_THEME_URL') ?: 'http://BANNER9_HOST:PORT/BannerExtensibility/theme')
-banner.theme.name=(System.getenv('BANNER_THEME_NAME') ?: 'ellucian')
-banner.theme.template=(System.getenv('BANNER_THEME_TEMPLATE') ?: 'BannerExtensibility')
-banner.theme.cacheTimeOut = (System.getenv('BANNER_THEME_CACHETIMEOUT') ?: 900)
+banner.theme.url = "<UPDATE_ME>" //Required only if theme server is remote. References the URL to the application hosting the Theme Server Example : . http://hostname:port/EmployeeSelfService/ssb/theme
+banner.theme.name = "<UPDATE_ME>" // This is the desired theme name to use. In a MEP environment, the application uses the MEP code as the theme name instead of the banner.theme.name . A theme by this name must be created in the Theme Editor on the server specified by banner.theme.url
+banner.theme.template = "EmployeeSelfService" // This is the name of the scss file containing the theme settings.
+                                              // EmployeeSelfService.war includes a template named “employeeselfservice.scss” at css/theme/employeeselfservice.scss.
+                                              //This file needs to be installed in the Theme Server’. Example : "employeeselfservice"
+banner.theme.cacheTimeOut=120 // seconds, required only if the app is theme server
 
 /** *****************************************************************************
  *                                                                              *
  *                          Google Analytics          *
  *                                                                              *
  ***************************************************************************** **/
- banner.analytics.trackerId=(System.getenv('BANNER_ANALYSTICS_TRACKERID') ?: '')
- banner.analytics.allowEllucianTracker=(System.getenv('BANNER_ANALYSTICS_ALLOWELLUCIANTRACKER') ? Boolean.parseBoolean(System.getenv('BANNER_ANALYSTICS_ALLOWELLUCIANTRACKER')) : true)
+banner.analytics.trackerId=
+banner.analytics.allowEllucianTracker=true
 
 
 /** ***************************************************************************
@@ -469,7 +471,7 @@ hibernate.cache.use_query_cache=true         // Default true. Make it false for 
  *           Home Page link when error happens during authentication.           *
  *                                                                              *
  ***************************************************************************** **/
-grails.plugin.springsecurity.homePageUrl=(System.getenv('GRAILS_PLUGIN_SPRINGSECURITY_HOMEPAGEURL') ?: 'http://URL:PORT/' )
+grails.plugin.springsecurity.homePageUrl='http://URL:PORT/'
 
 /** *****************************************************************************
  *                                                                              *
@@ -483,9 +485,8 @@ bdmserver {
                  AXWebServicesUrl 	= 'http://<UPDATE ME>/appxtender/axservicesinterface.asmx'
                  AXWebAccessURL  	= 'http://<UPDATE ME>/appxtender/'
                  Username       	= '<UPDATE ME>'
-                 Password        	= '<UPDATE ME>'
                  BdmDataSource     	= '<UPDATE ME>'
-                 KeyPassword     	= '<UPDATE ME>'
+                 Password           = ''            //Leave this password as empty String.
                  AppName         	= '<UPDATE ME>'
             }
 
@@ -495,3 +496,161 @@ bdmserver {
 ************************************************************************************** **/
 csod.base.dir = "Update the folder location where CSOD export files can be created before downloading"
 csod.file.delimiter = "|"
+
+/** *************************************************************************************
+*                                                                                       *
+*                    ++++Quartz scheduler Configurations ++++                           *
+************************************************************************************** **/
+configJob.delay=60000   //Time in milliseconds to configure when the quartz scheduler should start after the server startup, if its not configured then the default value is 60000.
+configJob.interval=60000 //Time in milliseconds to configure the interval at which the quartz schedule should run, default value is 60000 if not configured.
+configJob.actualCount=-1 //The count of number of times the config job would run.  If value is -1, the job will run indefinitely.  If the value is o, the job will not run.  Default value is -1 when not configured.
+
+
+/* Here are 3 patterns to use to configure the keys
+
+Pattern 1 - With key and value
+Syntax:
+ssconfig.app.seeddata.keys = [
+['<Key1>': <Boolean value>], ['<Key2>': <Boolean Value2>], ['<Key3>': '<String Value>'], ['<Key4>':<Numeric value>]
+]
+
+Pattern 2 - With key only (value derived from configuration files)
+Syntax:
+ssconfig.app.seeddata.keys = [
+['<Key1>'], ['<Key2>'], ['<Key3>'], ['<Key4>']
+]
+
+Pattern 3 - Combination of Pattern 1 and 2 - With key only and key/value pairs.
+Syntax:
+ssconfig.app.seeddata.keys = [
+['<Key1>': <Boolean value>], ['<Key2>'], ['<Key3>': '<String Value>'], ['<Key4>']
+]
+
+*/
+
+//Down below are keys configured using pattern 2
+ssconfig.app.seeddata.keys = [
+['ssbPassword.reset.enabled'],
+['defaultWebSessionTimeout'],
+['banner.picturesPath'],
+['banner.hr.employerLogoPath'],
+['banner.pdf.fopBaseUrl'],
+['banner8.SS.url'],
+['banner8.SS.addressUpdateURL'],
+['banner8.SS.addressUpdate'],
+['banner8.SS.emailUpdateURL'],
+['banner8.SS.emailUpdate'],
+['banner8.SS.emergencyContactUpdateURL'],
+['banner8.SS.emergencyContactUpdate'],
+['banner8.SS.benefitsEnrollmentURL'],
+['banner8.SS.benefitsEnrollment8xLinkAvailable'],
+['banner8.SS.currentBeneficiariesURL'],
+['banner8.SS.currentBeneficiaries8xLinkAvailable'],
+['banner8.SS.currentSummaryURL'],
+['banner8.SS.currentSummary8xLinkAvailable'],
+['banner8.SS.enterTimeURL'],
+['banner8.SS.enterTime8xLinkAvailable'],
+['banner8.SS.requestTimeOffURL'],
+['banner8.SS.requestTimeOff8xLinkAvailable'],
+['banner8.SS.electronicPersonalActionFormsURL'],
+['banner8.SS.facultyLoadCompensationURL'],
+['banner8.SS.facultyLoadCompensation8xLinkAvailable'],
+['banner8.SS.salaryPlannerURL'],
+['banner8.SS.salaryPlanner8xLinkAvailable'],
+['banner8.SS.effortReportingURL'],
+['banner8.SS.effortReporting8xLinkAvailable'],
+['banner8.SS.laborRedistributionURL'],
+['banner8.SS.laborRedistribution8xLinkAvailable'],
+['banner8.SS.benefitsAdministratorURL'],
+['banner8.SS.benefitsAdministrator8xLinkAvailable'],
+['banner8.SS.morePersonalInfoUpdateURL'],
+['banner8.SS.morePersonalInfoUpdate'],
+['banner8.SS.timeSheetURL'],
+['banner8.SS.timeSheet8xLinkAvailable'],
+['banner8.SS.leaveApprovalsURL'],
+['banner8.SS.leaveApprovals8xLinkAvailable'],
+['banner8.SS.campusDirectoryURL'],
+['banner8.SS.campusDirectory8xLinkAvailable'],
+['banner8.SS.employeeMenuURL'],
+['banner8.SS.employeeMenuLinkAvailable'],
+['banner8.SS.electronicW2ConsentURL'],
+['banner8.SS.electronicW2Consent8xLinkAvailable'],
+['banner8.SS.w2WageTaxStatementURL'],
+['banner8.SS.w2WageTaxStatement8xLinkAvailable'],
+['banner8.SS.w2CorrectedWageTaxStatementURL'],
+['banner8.SS.yearEnd1095CStatementLinkAvailable'],
+['banner8.SS.yearEnd1095CStatementURL'],
+['banner8.SS.yearEnd1094ReceiptIDEntryLinkAvailable'],
+['banner8.SS.yearEnd1094ReceiptIDEntryURL'],
+['banner8.SS.directDepositAllocationURL'],
+['banner8.SS.directDepositAllocation8xLinkAvailable'],
+['banner8.SS.taxReturnURL'],
+['banner8.SS.taxReturn8xLinkAvailable'],
+['banner8.SS.electronicTaxFormsConsentURL'],
+['banner8.SS.personalTaxCreditsSourceDeductionsURL'],
+['banner8.SS.openEnrollmentAdminURL'],
+['banner8.SS.openEnrollmentAdmin8xLinkAvailable'],
+['banner8.SS.openEnrollmentNewHireURL'],
+['banner8.SS.openEnrollmentNewHire8xLinkAvailable'],
+['banner8.SS.openEnrollmentURL'],
+['banner8.SS.openEnrollment8xLinkAvailable'],
+['banner8.SS.lifeEventChangeURL'],
+['banner8.SS.lifeEventChange8xLinkAvailable'],
+['banner.SS.directDepositURL'],
+['banner.SS.directDepositAppLinkAvailable'],
+['banner.SS.personalInfoURL'],
+['banner.SS.personalInfoAppLinkAvailable'],
+['banner.SS.deductionsHistoryLinkAvailable'],
+['ess.display.preferredFirstName'],
+['ess.displayComponent.Photo'],
+['ess.displayComponent.EmployeeStatus'],
+['ess.displayComponent.HomeOrganization'],
+['ess.displayComponent.DistOrganization'],
+['ess.displayComponent.Ecls'],
+['ess.displayComponent.PartTimeFullTimeIndicator'],
+['ess.displayComponent.OriginalHire'],
+['ess.displayComponent.CurrentHire'],
+['ess.displayComponent.AdjustedHire'],
+['ess.displayComponent.Seniority'],
+['ess.displayComponent.FirstWorkDay'],
+['ess.displayComponent.JobLocation'],
+['ess.displayComponent.College'],
+['ess.displayComponent.Campus'],
+['ess.displayComponent.DistrictOrDivision'],
+['ess.displayComponent.PositionSuffix'],
+['ess.displayComponent.Supervisor'],
+['ess.displayComponent.TimeSheetOrgn'],
+['ess.displayComponent.YtdEarnings'],
+['ess.displayComponent.BenefitsSection'],
+['ess.displayComponent.BeneficiaryLink'],
+['ess.displayComponent.TaxSection'],
+['ess.displayComponent.PaySection'],
+['ess.displayComponent.EarningsSection'],
+['ess.displayComponent.EmployeeSummarySection'],
+['ess.displayComponent.PayStubPdf'],
+['ess.displayComponent.JobSummary'],
+['ess.displayComponent.BirthDate'],
+['ess.displayComponent.HireDate'],
+['banner.theme.url'],
+['banner.theme.name'],
+['banner.theme.template'],
+['banner.theme.cacheTimeOut'],
+['banner.analytics.trackerId'],
+['banner.analytics.allowEllucianTracker'],
+['csod.base.dir'],
+['csod.file.delimiter'],
+]
+
+/**
+* make sure following properties are included as Global SeedData key for DB Configurations
+*/
+ssconfig.global.seeddata.keys = [
+['bdm.enabled'],
+['bdm.file.location'],
+['bdmserver.AXWebServicesUrl'],
+['bdmserver.AXWebAccessURL'],
+['bdmserver.Username'],
+['bdmserver.BdmDataSource'],
+['bdmserver.AppName'],
+['bdmserver.Password'],
+]
