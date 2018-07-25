b133f1a1776b90fbcd3b59653c28e8fb95ae16d6
Eric Allen <eric.allen@usu.edu>
2018-07-25 16:18:26 -0600

    appnav 3.0.0.2 config

--- WEB-INF/classes/applicationNavigator_configuration.groovy
+++ WEB-INF/classes/applicationNavigator_configuration.groovy
@@ -228,6 +228,7 @@ grails.plugin.springsecurity.logout.afterLogoutUrl = (System.getenv('GRAILS_PLUG
 // Can be institutional home page ex: http://myportal/main_page.html
 grails.plugin.springsecurity.homePageUrl=(System.getenv('GRAILS_PLUGIN_SPRINGSECURITY_HOMEPAGEURL') ?: 'http://APPLICATION_NAVIGATOR_HOST:PORT/applicationNavigator' )
 
+
 /********************************************************************************
  *                                                                              *
  *                             CAS SSO Configuration                            *
@@ -335,7 +336,7 @@ seamless.exposeMenu=true
 // Example: seamless.brandTitle=["Default": "Ellucian University","<MEP_CODE>":"<MEP_BRAND_TITLE>", "<MEP_CODE>":"<MEP_BRAND_TITLE>"]
 seamless.brandTitle=["Default": (System.getenv('SEAMLESS_BRANDTITLE') ?: "Ellucian University" )]
 
-seamless.sessionTimeout = (System.getenv('SEAMLESS_SESSIONTIMEOUT') ? Integer.parseInt(System.getenv('SEAMLESS_SESSIONTIMEOUT')) :30 )             // Session timeout in minutes. A value of -1 indicates session does not timeout
+seamless.sessionTimeout = (System.getenv('SEAMLESS_SESSIONTIMEOUT') ? Integer.parseInt(System.getenv('SEAMLESS_SESSIONTIMEOUT')) :30 )              // Session timeout in minutes. A value of -1 indicates session does not timeout
 seamless.sessionTimeoutNotification = (System.getenv('SEAMLESS_SESSIONTIMEOUTNOTIFICATION') ? Integer.parseInt(System.getenv('SEAMLESS_SESSIONTIMEOUTNOTIFICATION')) : 5 )  // Notification prompt x minutes before sessionTimeout
 
 // This list includes objects to be excluded from search
@@ -391,9 +392,9 @@ grails.plugin.xframeoptions.urlPattern = '/login/auth'
 
 
  /********************************************************************************
- *                                                                              *
+  *                                                                              *
  *              Google Analytics Configuration                                  *
  *                                                                              *
  ********************************************************************************/
-banner.analytics.trackerId=(System.getenv('BANNER_ANALYSTICS_TRACKERID') ?: '')
-banner.analytics.allowEllucianTracker=(System.getenv('BANNER_ANALYSTICS_ALLOWELLUCIANTRACKER') ? Boolean.parseBoolean(System.getenv('BANNER_ANALYSTICS_ALLOWELLUCIANTRACKER')): true) // true|false - default true
+ banner.analytics.trackerId=(System.getenv('BANNER_ANALYSTICS_TRACKERID') ?: '')              // institution's analytics tracker ID - blank by default
+ banner.analytics.allowEllucianTracker=(System.getenv('BANNER_ANALYSTICS_ALLOWELLUCIANTRACKER') ? Boolean.parseBoolean(System.getenv('BANNER_ANALYSTICS_ALLOWELLUCIANTRACKER')): true)  // true|false - default true
