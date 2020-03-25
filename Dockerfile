3e54f42ee9d648934136e7003fcb6d7e05bef334
Eric Allen <ericdallen@me.com>
2020-03-25 14:21:54 -0600

    udpated versions and added prep script

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM edurepo/banner9-admin:tomcat8.5.47-corretto8
-LABEL banner.common="9.3.18.0.10" \
-      banner.accountreceivable="9.3.12.0.2" \
+LABEL banner.common="9.3.18.3.2" \
+      banner.accountreceivable="9.3.13.0.2" \
       banner.documentmanagement="9.3.1.0.4" \
       banner.finance="9.3.13.0.5" \
-      banner.financialaid="9.3.17.0.3" \
+      banner.financialaid="9.3.18.0.2" \
       banner.general="9.3.15.0.6" \
-      banner.humanresources="9.3.12.0.6" \
+      banner.humanresources="9.3.13.0.2" \
       banner.positioncontrol="9.3.12.0.3" \
       banner.student="9.3.16.0.4" 
 
---
+++ prep.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+INSTANCE=wprod
+CLEANADDRESS=false
+echo "Removing old wars and folders"
+rm -rf BannerAdmin
+rm -rf BannerAdmin.ws
+rm -rf bannerHelp
+rm -rf BannerAdmin.war
+rm -rf BannerAdmin.ws.war
+rm -rf bannerHelp.war
+
+echo "Making new folders"
+mkdir BannerAdmin BannerAdmin.ws bannerHelp
+
+echo "Downloading files from build.banner"
+scp root@build.banner.usu.edu:/u01/deploy/$INSTANCE/BannerPages/BannerAdmin.war .
+scp root@build.banner.usu.edu:/u01/deploy/$INSTANCE/BannerPages/BannerAdmin.ws.war .
+scp root@build.banner.usu.edu:/u01/deploy/$INSTANCE/BannerPages/bannerHelp.war .
+
+echo "Extracting BannerAdmin"
+cd BannerAdmin
+unzip ../BannerAdmin.war
+cd ..
+
+echo "Extracting BannerAdmin.ws"
+cd BannerAdmin.ws
+unzip ../BannerAdmin.ws.war
+cd ..
+
+echo "Extracting bannerHelp"
+cd bannerHelp
+unzip ../bannerHelp.war
+cd ..
+
+if $CLEANADDRESS; then
+echo "Updating BannerAdmin.ws config for CleanAddress"
+CONFIGPROPFILEPATH="BannerAdmin.ws/WEB-INF/classes"
+if ! cat $CONFIGPROPFILEPATH/config.properties | tr -d [[:space:]] | grep -iq "plugins=com.runnertech.ext"; then
+cd BannerAdmin.ws/WEB-INF/classes
+echo -e "##############################\n# Plug-in Definition\n##############################\n\nplugins = com.runnertech.ext\n\n$(cat config.properties)" > config.properties
+cd ../../../
+else
+ echo "Plugin definition already in config.properties. Nothing to do."
+fi
+head -10 $CONFIGPROPFILEPATH/config.properties
+fi
