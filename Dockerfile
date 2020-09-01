65bcecaab645656080a23be34d78bec91dd7906f
Eric Allen <eric.allen@usu.edu>
2020-09-01 16:11:02 -0600

    adding changes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM bandock/banner9-admin:tomcat8.5.37-jre8-alpine
+FROM edurepo/banner9-admin:tomcat8.5.47-corretto8
 
 #Fix timezone
 USER root
@@ -8,4 +8,4 @@ RUN echo $TIMEZONE > /etc/timezone
 USER tomcat
 
 COPY --chown=tomcat:tomcat eTranscriptAPI /usr/local/tomcat/webapps/eTranscriptAPI
-COPY --chown=tomcat:tomcat ojdbc7.jar /usr/local/tomcat/lib/ojdbc6.jar
+#COPY --chown=tomcat:tomcat ojdbc7.jar /usr/local/tomcat/lib/ojdbc6.jar
--- WEB-INF/classes/eTranscriptAPI_configuration.groovy
+++ WEB-INF/classes/eTranscriptAPI_configuration.groovy
@@ -1,7 +1,7 @@
 /** ****************************************************************************
          Copyright 2017 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
-
+ 
 /** ****************************************************************************
  *                                                                              *
  *         ETranscriptAPI Configuration                                         *
@@ -14,140 +14,24 @@ This file contains configuration needed by the ETranscriptAPI Banner 9
 web application. Please refer to the administration guide for
 additional information regarding the configuration items contained within this file.
 
-This configuration file contains the following sections:
-
-	* JMX Bean Names
-
-    * Logging Configuration (Note: Changes here require restart -- use JMX to avoid the need restart)
 
      NOTE: DataSource and JNDI configuration resides in the cross-module
            'banner_configuration.groovy' file.
+
+ // Logging configuration has moved to logback.groovy file under <App>/grails-app/conf/logback.groovy
 ***************************************************************************** **/
 
-/** ****************************************************************************
- *                                                                             *
- *                              JMX Bean Names                                 *
- *                                                                             *
- *******************************************************************************/
+/** *****************************************************************************
+ *                                                                              *
+ *                      ConfigJob (Platform 9.23)                               *
+ *     Support for configurations to reside in the database.                    *
+ *                                                                              *
+ ***************************************************************************** **/
+configJob.delay = 60000
+configJob.interval = 12000
+configJob.actualCount = -1
 
-// The names used to register Mbeans must be unique for all applications deployed
-// into the JVM.  This configuration should be updated for each instance of each
-// application to ensure uniqueness.
-jmx {
-    exported {
-        log4j = "etranscript-api-log4j"
-    }
-}
 
-/** ****************************************************************************
- *                                                                             *
- *                          Logging Configuration                              *
- *                                                                             *
- **************************************************************************** **/
-// Note that logging is configured separately for each environment ('development', 'test', and 'production').
-// By default, all 'root' logging is 'off'.  Logging levels for root, or specific packages/artifacts, should be configured via JMX.
-// Note that you may enable logging here, but it:
-//   1) requires a restart, and
-//   2) will report an error indicating 'Cannot add new method [getLog]'. (although the logging will in fact work)
-//
-// JMX should be used to modify logging levels (and enable logging for specific packages). Any JMX client, such as JConsole, may be used.
-//
-// The logging levels that may be configured are, in order: ALL < TRACE < DEBUG < INFO < WARN < ERROR < FATAL < OFF
-
-// If we specify a 'logFileDir' as a system property, we'll write the file to that directory.
-// Otherwise, we'll log to the target/logs directory.
-
-log4j = {
-    def String loggingFileDir = System.properties['logFileDir'] ? "${System.properties['logFileDir']}" : "/usr/local/tomcat/logs"
-    def String logAppName      = "eTranscriptAPI"
-    def String loggingFileName = "${loggingFileDir}/${logAppName}.log".toString()
-    appenders {
-        rollingFile name:'appLog', file:loggingFileName, maxFileSize:"${10*1024*1024}", maxBackupIndex:10, layout:pattern( conversionPattern: '%d{[EEE, dd-MMM-yyyy @ HH:mm:ss.SSS]} [%t] %-5p %c %x - %m%n' )
-    }
-
-    switch( grails.util.Environment.current.name.toString() ) {
-        case 'development':
-            root {
-                error 'stdout','appLog'
-                additivity = true
-            }
-            info  'net.hedtech.banner.configuration.ApplicationConfigurationUtils'
-            error 'net.hedtech.banner.representations'
-            error 'net.hedtech.banner.supplemental.SupplementalDataService'
-			trace  'grails.app.controllers.net.hedtech.restfulapi.RestfulApiController'
-			trace  'grails.app.services'
-            break
-        case 'test':
-            root {
-                error 'stdout','appLog'
-                additivity = true
-            }
-            break
-        case 'production':
-            root {
-                error 'appLog'
-                additivity = true
-            }
-            info 'net.hedtech.banner.representations'
-            info 'net.hedtech.banner.supplemental.SupplementalDataService'
-            error 'grails.app.service'
-            error 'grails.app.controller'
-            break
-    }
-
-    // Log4j configuration notes:
-    // The following are some common packages that you may want to enable for logging in the section above.
-    // You may enable any of these within this file (which will require a restart),
-    // or you may add these to a running instance via JMX.
-    //
-    // Note that settings for specific packages/artifacts will override those for the root logger.
-    // Setting any of these to 'off' will prevent logging from that package/artifact regardless of the root logging level.
-
-    // ******** non-Grails classes (e.g., in src/ or grails-app/utils/) *********
-    off 'net.hedtech.banner.service'
-    off 'net.hedtech.banner.student'
-    off 'net.hedtech.banner.student.generalstudent'
-    off 'net.hedtech.banner.student.catalog'
-    off 'net.hedtech.banner.student.system'
-
-    // ******** Grails framework classes *********
-    off 'org.codehaus.groovy.grails.web.servlet'        // controllers
-    off 'org.codehaus.groovy.grails.web.pages'          // GSP
-    off 'org.codehaus.groovy.grails.web.sitemesh'       // layouts
-    off 'org.codehaus.groovy.grails.web.mapping.filter' // URL mapping
-    off 'org.codehaus.groovy.grails.web.mapping'        // URL mapping
-    off 'org.codehaus.groovy.grails.commons'            // core / classloading
-    off 'org.codehaus.groovy.grails.plugins'            // plugins
-    off 'org.codehaus.groovy.grails.orm.hibernate'      // hibernate integration
-    off 'org.springframework'                           // Spring IoC
-    off 'org.hibernate'                                 // hibernate ORM
-    off 'grails.converters'                             // JSON and XML marshalling/parsing
-    off 'grails.app.service.org.grails.plugin.resource' // Resource Plugin
-    off 'org.grails.plugin.resource'                    // Resource Plugin
-
-// ******* Security framework classes **********
-    all 'net.hedtech.banner.security'
-    off 'net.hedtech.banner.db'
-    all 'net.hedtech.banner.security.BannerAccessDecisionVoter'
-    all 'net.hedtech.banner.security.BannerAuthenticationProvider'
-    all 'net.hedtech.banner.security.CasAuthenticationProvider'
-    all 'net.hedtech.banner.security.SelfServiceBannerAuthenticationProvider'
-    off 'grails.plugins.springsecurity'
-    off 'org.springframework.security'
-    off 'org.apache.http.headers'
-    off 'org.apache.http.wire'
-
-    // Grails provides a convenience for enabling logging within artefacts, using 'grails.app.XXX'.
-    // Unfortunately, this configuration is not effective when 'mixing in' methods that perform logging.
-    // Therefore, for controllers and services it is recommended that you enable logging using the controller
-    // or service class name (see above 'class name' based configurations).  For example:
-    //     all  'net.hedtech.banner.testing.FooController' // turns on all logging for the FooController
-    //
-    // debug 'grails.app' // apply to all artefacts
-    // debug 'grails.app.<artefactType>.ClassName
-	off 'grails.app.controllers.net.hedtech.restfulapi.RestfulApiController'
-	off 'grails.app.services'
-}
 
 /* *****************************************************************************
  *                                                                             *
@@ -228,3 +112,10 @@ Note: It might also be necessary to increase transaction timeout to fetch more r
 //api.courses.page.maxUpperLimit=1000
 //api.sections.page.maxUpperLimit=1000
 //api."instructional-events".page.maxUpperLimit=1000
+
+/*********************************************************************************
+*                     Application Server Configuration                          *
+* When deployed on Tomcat this configuration should be targetServer="tomcat"    *
+* When deployed on Weblogic this configuration should be targetServer="weblogic"*
+*********************************************************************************/
+targetServer="tomcat"
