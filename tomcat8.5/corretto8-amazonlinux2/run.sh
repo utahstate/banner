669af108e6a5a9ccb48b0116b91c720accb0d1f9
Eric Allen <ericdallen@me.com>
2020-10-02 16:11:27 +0000

    Updated to tomcat 8.5.58 for corretto and run.sh to not set help api so that it uses the default ellucian site.

--- tomcat8.5/corretto8-amazonlinux2/run.sh
+++ tomcat8.5/corretto8-amazonlinux2/run.sh
@@ -29,7 +29,7 @@ setProperty() {
   if [ "$prop" = "banner9.baseurl" ]; then
     sed -i "s|^webapp\.location.*|webapp\.location = $val\/\${webapp.context}|g" /usr/local/tomcat/webapps/BannerAdmin.ws/WEB-INF/classes/config.properties
     sed -i "s|^webapp\.wrksp\.location.*|webapp\.wrksp\.location = $val\/\${webapp.wrksp.context}|g" /usr/local/tomcat/webapps/BannerAdmin.ws/WEB-INF/classes/config.properties
-    sed -i "s|<param name=\"APPNAV_HELP_URL\".*|<param name=\"APPNAV_HELP_URL\"   value=\"$val/bannerHelp/Main?page=\" />|g" /usr/local/tomcat/webapps/BannerAdmin/config.xml
+    #sed -i "s|<param name=\"APPNAV_HELP_URL\".*|<param name=\"APPNAV_HELP_URL\"   value=\"$val/bannerHelp/Main?page=\" />|g" /usr/local/tomcat/webapps/BannerAdmin/config.xml
     sed -i "s|<param name=\"APPNAV_API_URL\".*|<param name=\"APPNAV_API_URL\" value=\"$val/applicationNavigator/static/dist/m.js\" />|g" /usr/local/tomcat/webapps/BannerAdmin/config.xml
   fi
 
