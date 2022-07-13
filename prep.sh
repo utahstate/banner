7ab10c414361cce8eb7fbe0603dbc505661bfc04
Ken Andersen <ken.andersen@usu.edu>
2022-07-13 13:50:20 -0600

    Copy configs with prep 3.7

--- prep.sh
+++ prep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INSTANCE=zpprd
+INSTANCE=zdevl
 APP=applicationNavigator
 
 #Remove old war and app folder
@@ -27,5 +27,7 @@ cd ..
 cd saml
 unzip ../saml.zip
 cd ..
+
+cp WEB-INF/classes/* $APP/WEB-INF/classes/
 
 echo "$APP is ready for configuration"
